5418f7c6c0e222b097f3ada392c4b1198b4ef0ee
ramonepico <ramonepico@gmail.com>
2023-07-17 21:11:02 -0300

    finalizado

--- src/pages/CommentsPage.js
+++ src/pages/CommentsPage.js
@@ -3,29 +3,20 @@ import { BASE_URL } from "../constants/constants";
 import axios from "axios";
 import {Title,PostContainer } from '../style'
 import { Card } from '../components/Card/Card'
+import useRequestData from "../hooks/useRequestData";
 
 
 const  CommentsPage = () => {
-  const [postagens, setPostagens] = useState([]);
-
-
-  useEffect(() => {
-    axios
-      .get(`${BASE_URL}comments`)
-      .then((response) => {
-        setPostagens(response.data);
-      })
-      .catch((error) => {
-        console.log(error);
-      });
-  }, []);
+  
+  const [postagens, isLoading, isError] = useRequestData('comments')
 
   return (
     <div>
       <Title>Comentários dos usuários</Title>
       <PostContainer>
-
-      {postagens.map((post) => {
+      { isError ? <p>ERRO! TENTE NOVAMENTE.</p> :
+      isLoading ? <p>CARREGANDO...</p> :
+      postagens.map((post) => {
         //console.log(post);
         return(
           <Card 
--- src/pages/UserNamesPage.js
+++ src/pages/UserNamesPage.js
@@ -3,28 +3,20 @@ import { BASE_URL } from "../constants/constants";
 import axios from "axios";
 import {Title,NameContainer } from '../style'
 import { Card } from '../components/Card/Card'
+import useRequestData from "../hooks/useRequestData";
 
 
 const UserNamesPage = () => {
-  const [nomeUsuarios, setNomeUsuarios] = useState([]);
- 
-
-  useEffect(() => {
-    axios
-      .get(`${BASE_URL}users`)
-      .then((response) => {
-        setNomeUsuarios(response.data);
-      })
-      .catch((error) => {
-        console.log(error);
-      });
-  }, []);
+  
+  const [nomeUsuarios, isLoading, isError] = useRequestData('users')
 
   return (
     <div>
       <Title>Nomes dos usuários</Title>
       <NameContainer>
-        {nomeUsuarios.map((usuario) => {
+        { isError ? <p>ERRO! TENTE NOVAMENTE.</p>:
+        isLoading ? <p>CARREGANDO...</p> :
+        nomeUsuarios.map((usuario) => {
           return(
           <Card 
           key={usuario.id} 
